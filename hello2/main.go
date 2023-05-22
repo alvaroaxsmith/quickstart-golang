38b7579e55cf85f589ebfddb9ac25febe20271fd
alvaroaxsmith <alvaromachadoferreira@hotmail.com>
2023-05-22 13:52:56 +0000

    feat: add hello2

---
+++ hello2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  var name string = "Alvaro"
+  var idade int = 29
+  var version float32 = 0.1
+  fmt.Println("Hello Mr.",name,", you are", idade)
+  fmt.Println("This program is in version.", version)
+}
