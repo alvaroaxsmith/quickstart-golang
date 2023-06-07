ae619d7719884d30813b957bdc3d00d9f07669fc
alvaroaxsmith <alvaromachadoferreira@hotmail.com>
2023-06-07 22:38:26 +0000

    feat: add logs

--- src/main.go
+++ src/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,6 +17,7 @@ const delay = 5
 
 func main() {
 	displayIntroduction()
+
 	for {
 		displayMenu()
 
@@ -22,6 +28,7 @@ func main() {
 			startMonitoring()
 		case 2:
 			fmt.Println("Displaying Logs...")
+			printLogs()
 		case 0:
 			fmt.Println("Exiting the program")
 			os.Exit(0)
@@ -57,8 +64,7 @@ func readCommand() int {
 
 func startMonitoring() {
 	fmt.Println("Monitoring...")
-
-	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+	sites := readSitesFromFile()
 
 	for i := 0; i < monitorings; i++ {
 		for i, site := range sites {
@@ -73,11 +79,67 @@ func startMonitoring() {
 }
 
 func testSite(site string) {
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+	}
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "was loaded successfully!")
+		registerLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "is experiencing issues. Status Code:", resp.StatusCode)
+		registerLog(site, false)
 	}
+}
+
+func readSitesFromFile() []string {
+	var sites []string
+	file, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+	}
+
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+
+		sites = append(sites, line)
+
+		if err == io.EOF {
+			break
+		}
+
+	}
+
+	file.Close()
+	return sites
+}
+
+func registerLog(site string, status bool) {
+
+	file, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	file.Close()
+}
+
+func printLogs() {
+
+	file, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(file))
+
 }
